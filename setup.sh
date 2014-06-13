0580a2bf7dbd5648b6c5e72a0dcf610c965c2f13
Andrew Noble <aenoble@gmail.com>
2014-06-13 16:31:23 +0000

    moving pathogen.vim setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s /bin/zsh
 ln -s ~/beastmode.zsh-theme .oh-my-zsh/themes/
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
