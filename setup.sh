9226e2a8fc7222eb4cce6389d562a642da46c50f
Andrew Noble <aenoble@gmail.com>
2019-11-26 11:14:26 -0500

    Fixing setup script.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s /bin/zsh
-ln -s ~/beastmode.zsh-theme .oh-my-zsh/themes/
+# install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install tmux coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#fonts
+git clone https://github.com/powerline/fonts.git && bash ./fonts/install.sh && rm -rf fonts
+
+# link dotfiles
+ls -a1 dotfiles | grep '^\.[a-z]' | while read word; do ln -s ~/dotfiles/$word ~/$word; done
+ln -s ~/dotfiles/beastmode.zsh-theme ~/.oh-my-zsh/themes/
+
+vim +PluginInstall +qall
